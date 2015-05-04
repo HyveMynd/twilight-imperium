5017cd1b6b56718ed2b3499afb2818b2722ebd52
Andres Monroy <andresf.mon@gmail.com>
2015-05-04 00:15:50 -0600

    added dice config

--- client/diceRoller.js
+++ client/diceRoller.js
@@ -3,11 +3,30 @@
  */
 "use strict";
 Session.setDefault('results', []);
+Session.setDefault('diSideSelection', 0);
+Session.setDefault('diConfig', { sides: "6", numDi: 1});
+
+var numDi = (function () {
+    var result = [];
+    for (var i = 0; i < 15; i++){
+        result.push({value: i + 1});
+    }
+    return result;
+})();
 
 Template.rolldi.helpers({
-    result: function () {
+    lastResult: function () {
         var results = Session.get('results');
         return results[results.length-1];
+    },
+    numDi: function () {
+        return numDi;
+    },
+    diSides: function () {
+        return Session.get('diSideSelection');
+    },
+    diConfig: function () {
+        return Session.get('diConfig');
     }
 });
 
@@ -15,11 +34,24 @@ Template.rolldi.events({
     'click button': function () {
         // increment the counter when button is clicked
         var results = Session.get('results');
-        for (var i = 0; i < 100000; i++){
-            var result = Math.floor(Math.random() * 6) + 1;
+        var diConfig = Session.get('diConfig');
+        for (var i = 0; i < diConfig.numDi; i++){
+            var result = Math.floor(Math.random() * diConfig.sides) + 1;
             results.push(result);
         }
         Session.set('results', results);
+    },
+    'click paper-tab': function (event) {
+        var di = Session.get('diConfig');
+        di.sides = event.target.textContent;
+        Session.set('diConfig', di);
+    },
+    'core-select paper-dropdown-menu': function (event) {
+        if (event.originalEvent.detail.isSelected) {
+            var di = Session.get('diConfig');
+            di.numDi = event.originalEvent.detail.item.innerText;
+            Session.set('diConfig', di);
+        }
     }
 });
 
